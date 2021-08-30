ca08cef47258d5143a97f668fa04d98edde66926
Samuel Martinez <smartinez9535@gmail.com>
2021-08-30 14:12:33 -0600

    W1D1 Midday

--- week1/assignments/hoist.js
+++ week1/assignments/hoist.js
@@ -2,6 +2,10 @@
 console.log(hello);                                   
 var hello = 'world';                                 
 
+/* var hello
+console.log(hello);
+hello = 'world' */
+
 //2
 var needle = 'haystack';
 test();
@@ -9,6 +13,15 @@ function test(){
     var needle = 'magnet';
     console.log(needle);
 }
+
+/* var needle;
+needle = "haystack";
+function test(){
+    var needle;
+    needle = 'magnet';
+    console.log(needle); //magnet
+}
+test(); */
 
 //3
 var brendan = 'super cool';
@@ -18,10 +31,34 @@ function print(){
 }
 console.log(brendan);
 
+/* var brendan
+brendan = 'super cool'
+/* function print(){} */ //Seen but not called
+//console.log(brendan) //super cool */
+
 //4
+var food = 'chicken';
+console.log(food);
+eat();
+function eat(){
+    food = 'half-chicken';
+    console.log(food);
+    var food = 'gone';
+}
+
+//var food;
+//food = "chicken"
+//console.log(food) // "chicken"
+//function eat()
+//food = "half-chicken"
+//console.log(food) // "half-chicken"
+//var food;
+// food = "gone"
+
+//5
 mean();
 console.log(food);
-var mean = function() {
+var mean = function() { // var can't = function?
     food = "chicken";
     console.log(food);
     var food = "fish";
@@ -29,17 +66,17 @@ var mean = function() {
 }
 console.log(food);
 
-//5
-console.log(genre);
-var genre = "disco";
-rewind();
-function rewind() {
-    genre = "rock";
-    console.log(genre);
-    var genre = "r&b";
-    console.log(genre);
-}
-console.log(genre);
+//After running, realized mean is not defined, errors out
+//mean();
+//console.log(food); //undefined
+//var mean;
+//mean = function()
+//var food;
+//food = "chicken"
+//console.log(food)
+//food = "fish"
+//console.log(food) //"fish"
+//console.log(food) // undefined
 
 //6
 console.log(genre);
@@ -53,6 +90,18 @@ function rewind() {
 }
 console.log(genre);
 
+//console.log(genre) //undefined
+//var genre
+//genre = "disco"
+//function rewind();
+//var genre
+//genre = "rock"
+//console.log(genre) //rock
+//var genre
+//genre = "r&b"
+//console.log(genre) //"r&b"
+//console.log(genre) //"disco"
+
 //7
 dojo = "san jose";
 console.log(dojo);
@@ -64,6 +113,18 @@ function learn() {
     console.log(dojo);
 }
 console.log(dojo);
+
+//dojo
+//dojo = "san jose"
+//console.log(dojo) //"san jose"
+//learn()
+//dojo
+//dojo = "seattle"
+//console.log(dojo) //"seattle"
+//var dojo
+//dojo = "burbank" //"burbank"
+
+//console.log(dojo) //"san jose"
 
 //8
 console.log(makeDojo("Chicago", 65));
@@ -80,3 +141,27 @@ function makeDojo(name, students){
     }
     return dojo;
 }
+
+//console.log(makeDojo("Chicago", 65))
+//function makeDojo("Chicago", 65)
+//const dojo = {}
+//dojo.name
+//dojo.name = "Chicago"
+//dojo.students
+//dojo.students = 65
+//if(dojo.students > 50) //true
+//dojo.hiring
+//dojo.hiring = true
+//dojo
+
+//console.log(makeDojo("Berkeley", 0))
+//function makeDojo("Berkely", 0)
+//const dojo = {}
+//dojo.name
+//dojo.name = "Berkely"
+//dojo.students
+//dojo.students = 0
+//if(dojo.students > 50) //false
+//else if (dojo.students <= 0) //true
+// dojo = "closed for now" //after running, causes error as assigning to a const
+//"closed for now"
