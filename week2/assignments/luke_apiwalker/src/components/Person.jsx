e5e49f415f45de8a322bcfb91ddc8ef690055812
Samuel Martinez <smartinez9535@gmail.com>
2021-09-09 15:46:30 -0600

    W2D4 End

--- week2/assignments/luke_apiwalker/src/components/Person.jsx
+++ week2/assignments/luke_apiwalker/src/components/Person.jsx
@@ -1,16 +1,28 @@
-import React from 'react'
-import { useParams } from 'react-router'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
+import { useHistory } from 'react-router';
 
 const About = (props) => {
+// add in use effect functionality
+    const [person, setPerson] = useState({});
+    const history = useHistory();
 
+    //Query API for selected item
+    useEffect(() => 
+        axios.get(`https://swapi.dev/api/people/${props.id}`)
+        .then(res => {setPerson(res.data)})
+        .catch(err => history.push("/error"))
+    )
+    
+    //Display data retrieved from API
     return (
         <fieldset>
             <legend>Person.jsx</legend>
-                <h2 style={{textAlign: "left"}}>{props.person.name}</h2>
-                <h4 style={{textAlign: "left"}}>Height: {props.person.height} cm</h4>
-                <h4 style={{textAlign: "left"}}>Mass: {props.person.mass} kg</h4>
-                <h4 style={{textAlign: "left"}}>Hair Color: {props.person.hair_color}</h4>
-                <h4 style={{textAlign: "left"}}>Skin Color: {props.person.skin_color}</h4>
+                <h2 style={{textAlign: "left"}}>{person.name}</h2>
+                <h4 style={{textAlign: "left"}}>Height: {person.height} cm</h4>
+                <h4 style={{textAlign: "left"}}>Mass: {person.mass} kg</h4>
+                <h4 style={{textAlign: "left"}}>Hair Color: {person.hair_color}</h4>
+                <h4 style={{textAlign: "left"}}>Skin Color: {person.skin_color}</h4>
         </fieldset>
     )
 }
--- week2/assignments/luke_apiwalker/src/components/Planet.jsx
+++ week2/assignments/luke_apiwalker/src/components/Planet.jsx
@@ -1,16 +1,27 @@
-import React from 'react'
-import { useParams } from 'react-router'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
+import { useHistory } from 'react-router';
 
 const Result = (props) => {
+    const [planet, setPlanet] = useState({});
+    const history = useHistory();
 
+    //Query API for selected item
+    useEffect(() => 
+        axios.get(`https://swapi.dev/api/planets/${props.id}`)
+        .then(res => {setPlanet(res.data)})
+        .catch(err => history.push("/error"))
+    )
+
+    //Display data retrieved from API
     return (
         <fieldset>
             <legend>Planet.jsx</legend>
-                <h2 style={{textAlign: "left"}}>{props.planet.name}</h2>
-                <h4 style={{textAlign: "left"}}>Climate: {props.planet.climate}</h4>
-                <h4 style={{textAlign: "left"}}>Terrain: {props.planet.terrain}</h4>
-                <h4 style={{textAlign: "left"}}>Surface Water: {props.planet.surface_water}</h4>
-                <h4 style={{textAlign: "left"}}>Population: {props.planet.population}</h4>
+                <h2 style={{textAlign: "left"}}>{planet.name}</h2>
+                <h4 style={{textAlign: "left"}}>Climate: {planet.climate}</h4>
+                <h4 style={{textAlign: "left"}}>Terrain: {planet.terrain}</h4>
+                <h4 style={{textAlign: "left"}}>Surface Water: {planet.surface_water}</h4>
+                <h4 style={{textAlign: "left"}}>Population: {planet.population}</h4>
         </fieldset>
     )
 }
