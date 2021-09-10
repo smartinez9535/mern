cce96155e81a31605d644d773b7afcbfbdfa7f37
Samuel Martinez <smartinez9535@gmail.com>
2021-09-10 14:34:50 -0600

    W2D5 End

--- week2/algos/algos.js
+++ week2/algos/algos.js
@@ -238,4 +238,62 @@ function orderedMultisetUnion(sortedA, sortedB) {
     return newArr;
 }
 
-console.log(orderedMultisetUnion(arrA, arrB))
+//console.log(orderedMultisetUnion(arrA, arrB))
+
+
+// This is an actual interview algorithm given to a Coding Dojo alum
+
+// Find Consecutive Sums
+
+// You are given arr, a list of positive integers 0-255
+// You are given k, a integer between 1-255
+
+// Find all the consecutive groups of integers that add up to the value k
+
+// inputs
+// k = 16
+// arr = [2, 5, 3, 6, 7, 0, 0, 23, 11]
+
+// outputs
+// [
+//   [2, 5, 3, 6],
+//   [3, 6, 7]  // 3, 6, 7 appear consecutively, so they are including in the solution
+//   [3, 6, 7, 0],
+//   [3, 6, 7, 0, 0]
+// ]
+
+// create new arrays
+// if no matches, return empty array
+
+function findConsecutiveSums(arr, k) {
+    let result = [];
+    let group = [];
+    let temp;
+
+    for(let x = 0; x < arr.length; x++){
+        group.push(arr[x]);
+        temp = arr[x];
+
+        for(let y = x + 1; y < arr.length; y++){
+            if (temp > k){
+                //console.log("over")
+                group = [];
+                break;
+            }
+            else if (temp == k){
+                //console.log("added: " + group)
+                result.push([...group]);
+                group.push(arr[y]);
+                temp += arr[y];
+            }
+            else{
+                group.push(arr[y]);
+                temp += arr[y];
+            }
+            
+        }
+    }
+    return result;
+}
+
+console.log(findConsecutiveSums([2,5,3,6,7,0,0,23,11], 16));
