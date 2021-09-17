537e90f40a22491f649f0fa74e4cf5379f59698f
Samuel Martinez <smartinez9535@gmail.com>
2021-09-17 12:24:22 -0600

    W3D5 End

--- week3/lessons/boilerplate/server/config/mongoose.config.js
+++
@@ -1,8 +0,0 @@
-//1. import mongoose
-const mongoose = require("mongoose");
-const DB = "sept_notes_db";
-
-//2. connect mongoose to MongoDB
-mongoose.connect("mongodb://localhost/" + DB)
-    .then( () => console.log(`--> connected to ${DB} db`))
-    .catch( (err) => console.log(`error connecting to ${DB}`, err))
--- week3/lessons/boilerplate/server/routes/routes.js
+++
@@ -1,9 +0,0 @@
-const Note = require("../controllers/Note.controller");
-
-module.exports = (app) => {
-    app.get("/api/notes", Note.findAll);
-    app.get("/api/notes/:id", Note.findOne);
-    app.post("/api/notes", Note.create);
-    app.put("/api/notes/:id", Note.update);
-    app.delete("/api/notes/:id", Note.delete);
-}
